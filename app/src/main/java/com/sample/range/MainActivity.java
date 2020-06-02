288b4df5d796e79b16b81362c1a9178d93e0e245
ljhymca <ljhymca@naver.com>
2020-06-02 16:38:14 +0900

    범위 상단알림기능 구현중

--- app/src/main/java/com/sample/range/MainActivity.java
+++ app/src/main/java/com/sample/range/MainActivity.java
@@ -1,43 +1,29 @@
 package com.sample.range;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.core.app.NotificationCompat;
 
-import android.Manifest;
-import android.app.Activity;
-
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.naver.maps.geometry.LatLng;
-import com.naver.maps.geometry.Utmk;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
-import com.naver.maps.map.overlay.CircleOverlay;
-import com.naver.maps.map.overlay.LocationOverlay;
 import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.util.FusedLocationSource;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -48,8 +34,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     TextView sizeBarView;
     static int counter = 0;
 
+    NotificationManager manager;
+    private static String CHANNEL_ID = "channel1";
+    private static String CHANNEL_NAME = "Channel1";
+
     private FusedLocationSource locationSource;
     private NaverMap naverMap;
+    //위도와 경도 초기화
+    double latitude = 0;
+    double longitude = 0;
+    Marker marker2;
+    AlertReceiver receiver;
+    PendingIntent proximityIntent;
+    boolean inside = true;
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
@@ -59,102 +56,63 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* MapFragment mapFragment= (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);//지도 객체 생성
-
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);//지도 객체 생성
         if (mapFragment == null) {
             mapFragment = MapFragment.newInstance();
             getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
-
         }
         mapFragment.getMapAsync(this);
-        */
-        MapFragment mapFragment= (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);//지도 객체 생성
-        mapFragment.getMapAsync(this);
+
+        //   LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //   manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10000,0,(LocationListener)this);
 
     }
 
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-
-        locationSource = new FusedLocationSource(this,ACCESS_LOCATION_PERMISSION_REQUEST_CODE); //위치코드
+        locationSource = new FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE); //위치코드
         naverMap.setLocationSource(locationSource); //지도상의 로케이션 활성화
 
         UiSettings uiSettings = naverMap.getUiSettings(); //ui 세팅에 대한 객체
         uiSettings.setLocationButtonEnabled(true);
 
-        Marker marker2 = new Marker();
-        marker2.setPosition(new LatLng(1, 1));//일단 마커 위치지정
-        marker2.setMap(naverMap);//마커 생성
-        CircleOverlay markerCircle = new CircleOverlay();//원 생성
 
-        LocationOverlay locationOverlay = naverMap.getLocationOverlay();//오버레이 생성
-        locationOverlay.setVisible(true);//오버레이생성
-        locationOverlay.setPosition(new LatLng(37.5670135, 126.9783740));//좌표지정
-        //SeekBar할당
-        SeekBar sizeBar = (SeekBar) findViewById(R.id.sizeBar);
-        final TextView sizeBarView =(TextView)findViewById(R.id.sizeBarView);
-
-        sizeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        naverMap.addOnLocationChangeListener(new NaverMap.OnLocationChangeListener() { // 실시간 위치를 토스트를 통해서 위도,경도를 표시해주는 곳.
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                sizeBarView.setText(""+progress);
-                markerCircle.setRadius(progress);//원 반지름
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            public void onLocationChange(@NonNull Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
 
             }
         });
 
+        //여기서 타이머 설정 해서 넘겨주면 될 듯
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() { //타이머
+                databaseReference.child("id").child("myposition").child("lat").setValue(latitude, counter);
+                databaseReference.child("id").child("myposition").child("lon").setValue(longitude, counter);
+                //  Log.e("카운터:", String.valueOf(counter)); //2초마다 출력하기, 로그 캣에
+                counter++;
 
-        naverMap.setOnMapClickListener((point, coord)-> { //지도 화면클릭시
-            //Toast.makeText(this, getString(R.string.format_map_click, coord.latitude, coord.longitude), Toast.LENGTH_SHORT).show();
-            String progress = sizeBarView.getText().toString();
-            final long range = Long.parseLong(progress);
-            marker2.setPosition(new LatLng(coord.latitude, coord.longitude));//클릭 좌표로 마커 위치 이동
-            markerCircle.setCenter(coord);//원 중심을 선택한 위치로
-            markerCircle.setRadius(range);//원 반지름
-            markerCircle.setColor(0x4000FFFF);//불투명도 지정
-            markerCircle.setMap(naverMap);
-        //   sendbutton = (Button) findViewById(R.id.sendButton);
-            long now = System.currentTimeMillis();//시간생성
-            Date date = new Date(now);
-            SimpleDateFormat Now = new SimpleDateFormat("yyyyMMddHHmmss");
-            String formatDate = Now.format(date);
+                if (inside = true)
+                    if (37.64775518225889 < latitude && latitude < 37.64955181282713)
+                        if (127.06321675379839 < longitude && longitude < 127.0654858821362) {
+                            Log.e("범위", "범위내");
+                            showNoti();
+                            inside = false;
+                        } else if (inside = false)
+                            if (latitude > 37.64955181282713 || latitude < 37.64775518225889)
+                                if (longitude > 127.0654858821362 || longitude < 127.06321675379839) {
+                                    Log.e("범위", "범위밖");
+                                    inside = true;
+                                }
 
-
-
-            //여기서 타이머 설정 해서 넘겨주면 될 듯
-            TimerTask tt = new TimerTask() {
-                @Override
-                public void run() { //타이머
-                    databaseReference.child("id").child(formatDate).child("latitude").setValue(coord.latitude,counter);//위도
-                    databaseReference.child("id").child(formatDate).child("longitude").setValue(coord.longitude,counter);//경도
-                    databaseReference.child("id").child(formatDate).child("range").setValue(range,counter);//원 범위
-                    databaseReference.child("id").child("position").setValue(locationSource,counter);
-                   Log.e("카운터:", String.valueOf(counter)); //2초마다 출력하기, 로그 캣에
-                    counter++;
-
-                }
-            };
-
-            Timer timer = new Timer(); //타이머
-            timer.schedule(tt, 0, 2000);//2초 설정
-
-               //     databaseReference.child("id").child(formatDate).child("latitude").setValue(coord.latitude);//위도
-               //     databaseReference.child("id").child(formatDate).child("longitude").setValue(coord.longitude);//경도
-            // databaseReference.child("id").child(formatDate).child("range").setValue(range);//원 범위
-                    //id 값 넣을 자리
-
-
-        });
+            }
+        };
+        Timer timer = new Timer(); //타이머
+        timer.schedule(tt, 0, 2000);//2초 설정
 
 
     }
@@ -168,5 +126,94 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults);
                 return;
         }
+    }
+
+    private class AlertReceiver {
+    }
+
+
+    // 경도, 위도를 분석하여 근접 알람을 알려주는 구간
+   /* (marker2 != null) {
+        try {
+            if() {
+                locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 0, this);
+                isLocRequested = true;
+            }
+            else
+                Toast.makeText(this, "Permission이 없습니다..", Toast.LENGTH_LONG).show();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    } else if (view.getId() == R.id.alert) {
+        // 근접 경보를 받을 브로드캐스트 리시버 객체 생성 및 등록
+        // 액션이 kr.ac.koreatech.msp.locationAlert인 브로드캐스트 메시지를 받도록 설정
+        receiver = new AlertReceiver();
+        IntentFilter filter = new IntentFilter("com.sample.range.MainActivity");
+        registerReceiver(receiver, filter);
+
+        // ProximityAlert 등록을 위한 PendingIntent 객체 얻기
+        Intent intent = new Intent("com.sample.range.MainActivity");
+        proximityIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        try {
+            // 근접 경보 등록 메소드
+            // void addProximityAlert(double latitude, double longitude, float radius, long expiration, PendingIntent intent)
+            // 아래 위도, 경도 값의 위치는 2공학관 420호 창가 부근
+
+            locationSource.addProximityAlert(latitude, longtitude, 20, -1, proximityIntent);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        isAlertRegistered = true;
+    }else if(view.getId() == R.id.alert_release){
+        // 자원 사용 해제
+        try {
+            if(isAlertRegistered) {
+                locManager.removeProximityAlert(proximityIntent);
+                unregisterReceiver(receiver);
+            }
+            Toast.makeText(getApplicationContext(),"근접 경보 해제 되었습니다.",Toast.LENGTH_SHORT).show();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        // 자원 사용 해제
+        try {
+            if(isLocRequested) {
+                locManager.removeUpdates(this);
+                isLocRequested = false;
+            }
+            if(isAlertRegistered) {
+                locManager.removeProximityAlert(proximityIntent);
+                unregisterReceiver(receiver);
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+*/
+    public void showNoti() {
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (manager.getNotificationChannel(CHANNEL_ID) == null) {
+                manager.createNotificationChannel(new NotificationChannel(
+                        CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
+                ));
+
+                builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+            }
+        } else {
+            builder = new NotificationCompat.Builder(this);
+        }
+        builder.setContentTitle("안되는이유좀");
+        builder.setContentText("왜안되냐");
+        builder.setSmallIcon(android.R.drawable.ic_menu_view);
+        Notification noti = builder.build();
+
+        manager.notify(1, noti);
     }
 }
